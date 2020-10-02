63ec5b5f80dac1df981d6b60c37a879a6f89bc12
Abhinav Kolli <kolliganti@icloud.com>
2020-10-02 08:53:58 -0500

    Add new map for itinerary

---
+++ GitUseExampleMobileApps/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExampleMobileApps
+//
+//  Created by Kolli, Abhinav R on 10/2/20.
+//  Copyright © 2020 Kolli, Abhinav R. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
